1a80e72e2029fc2024463d84c12f5e42a3041e21
AadityaSubedi <Subediaaditya007@gmail.com>
2023-02-20 17:08:37 +0545

    remove unnecessary configurational variable

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-API_KEY = "b3d9b7d79a7047d1a99392c68c95d4f7"
-CLIENT_ID="1020532829824-8cir171h9s0426f122etjm7ai2795dsm.apps.googleusercontent.com"
-CLIENT_SECRET="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"
-GRANT_TYPE="authorization_code"
-REDIRECT_URI="http://localhost:8000/oauth2callback"
-SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-ACCESS_TOKEN = "ya29.a0AVvZVsqGbd84ZGiw3s1NirSLuuWdPUbGRNlTayiJNkIuTY4goFLCNhh4-1pnczcxd799uLYc31qsot5B2XB_zuzIUhenFENFxOrjXTkDKxUsxDuCbwLTXnxNSwSwzYbCYPK2by7sD7xx811J2PqfV2gp65bBaCgYKAYsSARESFQGbdwaIg8YPVQMFoxliwrnunWhOMQ0163"
 
 
 from pydantic import BaseSettings
